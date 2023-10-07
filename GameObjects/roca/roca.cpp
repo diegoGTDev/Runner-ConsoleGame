885d76f40ca7e3234b9731d8b92b83f15e5a6168
diegodev <andreelopez334@gmail.com>
2023-10-07 09:33:21 -0600

    Bug Fix: Roca

--- GameObjects/roca/roca.cpp
+++ GameObjects/roca/roca.cpp
@@ -1,29 +1,30 @@
 #include "./roca.h"
 #include "conio.h"
-#include<iostream>
+#include <iostream>
 #include "roca.h"
-#include<cmath>
-//Fordebug
-#include<windows.h>
-void ROCA::Render(){
+#include "../../Console/functions.h"
+#include <cmath>
+// Fordebug
+#include <windows.h>
+void ROCA::Render()
+{
     gotoxy(x, y);
-    std::cout<<"      ";
+    std::cout << "      ";
     gotoxy(x, y);
     printf("\033[31m%c%c\033[0m", 219, 220);
     //*For Debug
     gotoxy(4, 0);
-    std::cout<<"x: "<<x<<" y: "<<y<<"       ";
+    std::cout << "x: " << x << " y: " << y << "       ";
     gotoxy(60, 0);
-    std::cout<<"MIN_X: "<<MIN_X_MARCO;
-    std::cout<<" MAX_X: "<<MAX_X_MARCO;
+    std::cout << "MIN_X: " << MIN_X_MARCO;
+    std::cout << " MAX_X: " << MAX_X_MARCO;
     //**End for debug
-
 }
 
 void ROCA::Update(double elapsedSeconds)
 {
 
-    x -= velocity;   
+    x -= velocity;
 }
 
 void ROCA::HandleEvents()
@@ -31,8 +32,8 @@ void ROCA::HandleEvents()
     estaEnLimite();
 }
 
-
-int ROCA::getX(){
+int ROCA::getX()
+{
     return x;
 }
 
@@ -40,13 +41,38 @@ int ROCA::getY()
 {
     return y;
 }
-void ROCA::estaEnLimite(){
-    const int limite = MIN_X_MARCO;
-    //Si 20 es 
-    if (x >=limite && x <= limite){
+void ROCA::estaEnLimite()
+{
+    int limite = MIN_X_MARCO;
+    // Si 5 es mayor o igual que 6 y 8 es menor o igual que 6
+    const int _max_x_marco = MAX_X_MARCO - 1;
+    // if (!isOdd(_max_x_marco))
+    // {
+    //     if (x >= limite-1 && x <= limite-1)
+    //     {
+    //         gotoxy(x, y);
+    //         std::cout << "   ";
+    //         x = inicialX;
+    //         Sleep(1000);
+    //     }
+    // }else{
+    //     if (x >= limite && x <= limite)
+    //     {
+    //         gotoxy(x, y);
+    //         std::cout << "   ";
+    //         x = inicialX;
+    //         Sleep(1000);
+    //     }
+    // }
+
+    if (!isOdd(_max_x_marco)){
+        limite -= 1;
+    }
+    if (x >= limite && x <= limite)
+    {
         gotoxy(x, y);
-        std::cout<<"   ";
+        std::cout << "   ";
         x = inicialX;
+        Sleep(1000);
     }
 }
-
